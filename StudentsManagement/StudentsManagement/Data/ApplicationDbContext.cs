c8167653005ab5247a2abcd1b3d9a6850b129cff
Yassine <yassineelbrahmi11@gmail.com>
2024-12-25 14:54:15 +0100

    Migrations

--- StudentsManagement/StudentsManagement/Data/ApplicationDbContext.cs
+++ StudentsManagement/StudentsManagement/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using StudentManagamentShared.Models;
 using StudentsManagement.Shared.Models;
 
 namespace StudentsManagement.Data   
@@ -7,5 +8,11 @@ namespace StudentsManagement.Data
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
         public DbSet<Student> students { get; set; }
+        public DbSet<SystemeCode> SystemeCodes { get; set; }
+
+        public DbSet<Country> Countries { get; set; }
+
+        public DbSet<SystemCodeDetails> SystemCodeDetails { get; set; }
+
     }
 }
