608de73f8abbcbfe093d03bd02fc154c4d29cc1d
Yassine <yassineelbrahmi11@gmail.com>
2024-12-25 14:52:52 +0100

    Add New Models

---
+++ StudentManagamentShared/Models/SystemCodeDetails.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagamentShared.Models
+{
+    public class SystemCodeDetails
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int SystemCodeId { get; set; }
+
+        public SystemeCode SystemCode { get; set; }
+
+        public string Code { get; set; }
+
+        public string Description { get; set; }
+
+        public int? OrderNo { get; set; }
+    }
+}
---
+++ StudentManagamentShared/Models/SystemeCode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagamentShared.Models
+{
+    public class SystemeCode
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Description { get; set; }
+
+    }
+}
