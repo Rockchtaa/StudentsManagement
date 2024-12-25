09e88679fc0add1eb453036e5b5f6b90a3108ceb
Yassine <yassineelbrahmi11@gmail.com>
2024-12-25 21:54:53 +0100

    Country Interface added

---
+++ StudentManagamentShared/StudentRepository/ICountryRepository.cs
@@ -0,0 +1,18 @@
+﻿using StudentManagamentShared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagamentShared.StudentRepository
+{
+    public interface ICountryRepository
+    {
+        Task<List<Country>> GetCountriesAsync();
+        Task<Country> GetCountryByIdAsync(int id);
+        Task<Country> AddCountryAsync(Country country);
+        Task<Country> UpdateCountryAsync(Country country);
+        Task<bool> DeleteCountryAsync(int id);
+    }
+}
