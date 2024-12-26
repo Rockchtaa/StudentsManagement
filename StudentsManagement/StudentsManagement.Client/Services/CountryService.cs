73d0bb2a78420d8f7c36060b99b8f7e365cf5d1a
Yassine <yassineelbrahmi11@gmail.com>
2024-12-26 20:33:41 +0100

    service for country and system

--- StudentsManagement/StudentsManagement.Client/Services/CountryService.cs
+++ StudentsManagement/StudentsManagement.Client/Services/CountryService.cs
@@ -13,34 +13,34 @@ namespace StudentsManagement.Client.Services
             this._httpClient = httpClient;
         }
 
-        public Task<Country> AddCountryAsync(Country country)
+        public async Task<Country> AddCountryAsync(Country country)
         {
             var countryResponse = _httpClient.PostAsJsonAsync("api/Country/AddCountry", country);
-            return countryResponse.Result.Content.ReadFromJsonAsync<Country>();
+            return await countryResponse.Result.Content.ReadFromJsonAsync<Country>();
         }
 
-        public Task<bool> DeleteCountryAsync(int id)
+        public async Task<bool> DeleteCountryAsync(int id)
         {
-            var countryResponse = _httpClient.DeleteAsync($"api/Country/DeleteCountry/{id}");
-            return countryResponse.Result.Content.ReadFromJsonAsync<bool>();
+            var countryResponse =  _httpClient.DeleteAsync($"api/Country/DeleteCountry/{id}");
+            return await countryResponse.Result.Content.ReadFromJsonAsync<bool>();
         }
 
-        public Task<List<Country>> GetCountriesAsync()
+        public async Task<List<Country>> GetCountriesAsync()
         {
-            var allCountries = _httpClient.GetFromJsonAsync<List<Country>>("api/Country/GetCountries");
+            var allCountries = await _httpClient.GetFromJsonAsync<List<Country>>("api/Country/GetCountries");
             return allCountries;
         }
 
-        public Task<Country> GetCountryByIdAsync(int id)
+        public async Task<Country> GetCountryByIdAsync(int id)
         {
-            var singleCountry = _httpClient.GetFromJsonAsync<Country>($"api/Country/GetSingleCountry/{id}");
+            var singleCountry = await _httpClient.GetFromJsonAsync<Country>($"api/Country/GetSingleCountry/{id}");
             return singleCountry;
         }
 
-        public Task<Country> UpdateCountryAsync(Country country)
+        public async Task<Country> UpdateCountryAsync(Country country)
         {
             var countryResponse = _httpClient.PutAsJsonAsync("api/Country/UpdateCountry", country);
-            return countryResponse.Result.Content.ReadFromJsonAsync<Country>();
+            return await countryResponse.Result.Content.ReadFromJsonAsync<Country>();
         }
     }
 }
---
+++ StudentsManagement/StudentsManagement.Client/Services/SystemCodeService.cs
@@ -0,0 +1,48 @@
+﻿using StudentManagamentShared.Models;
+using StudentManagamentShared.StudentRepository;
+using System.Net.Http.Json;
+
+namespace StudentsManagement.Client.Services
+{
+    public class SystemCodeService : ISystemCodeRepository
+    {
+        private readonly HttpClient _httpClient;
+        public SystemCodeService(HttpClient httpClient)
+        {
+            this._httpClient = httpClient;
+        }
+
+        public async Task<SystemeCode> AddAsync(SystemeCode systeme)
+        {
+            var systemeResponse = await _httpClient.PostAsJsonAsync("api/SystemeCode/AddSystemeCode", systeme);
+            return await systemeResponse.Content.ReadFromJsonAsync<SystemeCode>();
+        }
+
+        public async Task<List<SystemeCode>> GetAllAsync()
+        {
+            var allSysteme = await _httpClient.GetFromJsonAsync<List<SystemeCode>>("api/SystemeCode/GetSystemeCodes");
+            return allSysteme;
+
+        }
+
+        public async Task<SystemeCode> GetByIdAsync(int id)
+        {
+            var singleSysteme = await _httpClient.GetFromJsonAsync<SystemeCode>($"api/SystemeCode/GetSingleSystemeCode/{id}");
+            return singleSysteme;
+          
+        }
+
+        public async Task<SystemeCode> UpdateAsync(SystemeCode systeme)
+        {
+            var systemeResponse = _httpClient.PutAsJsonAsync("api/SystemeCode/UpdateSystemeCode", systeme);
+            return await systemeResponse.Result.Content.ReadFromJsonAsync<SystemeCode>();
+        }
+
+        Task<SystemeCode> ISystemCodeRepository.DeleteAsync(int id)
+        {
+            var systemeResponse = _httpClient.DeleteAsync($"api/SystemeCode/DeleteSystemeCode/{id}");
+            return systemeResponse.Result.Content.ReadFromJsonAsync<SystemeCode>();
+           
+        }
+    }
+}
