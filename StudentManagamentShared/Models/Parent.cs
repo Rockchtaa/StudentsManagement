aed36716ca8d2d3817eaaf6bcff2688f45d40a43
Yassine <yassineelbrahmi11@gmail.com>
2024-12-28 16:54:58 +0100

    models adjusted with foreign keys

---
+++ StudentManagamentShared/Models/Parent.cs
@@ -0,0 +1,34 @@
+﻿using StudentManagamentShared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsManagement.Shared.Models
+{
+    public class Parent
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+
+        public string Gender { get; set; }
+
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+
+        public int StudentId { get; set; }
+
+        public Student Student { get; set; }
+
+        public int ParentTypeId { get; set; }
+
+        public SystemCodeDetails ParentType { get; set; }
+
+        public DateTime DOB { get; set; }
+    }
+}
--- StudentManagamentShared/Models/Student.cs
+++ StudentManagamentShared/Models/Student.cs
@@ -8,10 +8,13 @@ namespace StudentsManagement.Shared.Models
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+
         public string EmailAddress { get; set; }
         public string PhoneNumber { get; set; }
         public string Address { get; set; }
         public string Country { get; set; }
+
 
     }
 }
