d518570b6ba3916952c029ddeb306f907eae011e
Yassine <yassineelbrahmi11@gmail.com>
2024-12-28 16:55:11 +0100

    change name of Repo

---
+++ StudentManagamentShared/Repository/ISystemCodeDetailsRepository.cs
@@ -0,0 +1,21 @@
+﻿using StudentManagamentShared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagamentShared.StudentRepository
+{
+    public interface ISystemCodeDetailsRepository
+    {
+        Task<SystemCodeDetails> AddAsync(SystemCodeDetails mod);
+        Task<SystemCodeDetails> UpdateAsync(SystemCodeDetails mod);
+        Task<SystemCodeDetails> DeleteAsync(int id);
+        Task<SystemCodeDetails> GetByIdAsync(int id);
+        Task<List<SystemCodeDetails>> GetAllAsync();
+        Task<List<SystemCodeDetails>> GetByCodeAsync(string code);
+
+
+    }
+}
